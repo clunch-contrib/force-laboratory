0c8fada9a74a8370bfd044868cb66de64949ec43
你好2007 <2501482523@qq.com>
2021-08-29 13:03:24 +0800

    力布局用例开发完毕

---
+++ README.md
@@ -0,0 +1,32 @@
+# force-laboratory
+力图实验用例，基于image2D开发，用于探索力图算法的最佳实践。
+
+<p align='center'>
+    <img src='./force.gif'>
+</p>
+
+## 如何启动本地编辑？
+
+首先，你需要确保本地安装了node.js，然后，执行下面命令安装项目依赖：
+
+```bash
+npm install
+```
+
+接着，启动下面命令会自动打开页面，修改内容页面也自动刷新：
+
+```bash
+npm run dev
+```
+
+如果你想发布开发的代码，执行下面命令进行打包：
+
+```bash
+npm run build
+```
+
+开源协议
+---------------------------------------
+[Apache-2.0](https://github.com/clunch-contrib/force-laboratory/blob/master/LICENSE)
+
+Copyright (c) 2021 [hai2007](https://hai2007.gitee.io/sweethome/) 走一步，再走一步。
---
+++ src/region.js
@@ -0,0 +1,83 @@
+// 用特定色彩绘制区域
+let _drawerRegion = function (pen, color, drawback, regionManger) {
+    pen.beginPath();
+    pen.fillStyle = color;
+    pen.strokeStyle = color;
+    if (typeof drawback != "function") return pen;
+    drawback(pen);
+    return regionManger;
+};
+
+// 区域对象，用于存储区域信息
+// 初衷是解决类似canvas交互问题
+// 可以用于任何标签的区域控制
+
+// 由于实验的画布是canvas，特地为image2D扩展了区域，方便获取当前鼠标拖拽的结点是谁
+
+export default function () {
+
+    let regions = {},//区域映射表
+        canvas = document.createElement('canvas'),
+        rgb = [0, 0, 0],//区域标识色彩,rgb(0,0,0)表示空白区域
+        p = 'r';//色彩增值位置
+
+    canvas.setAttribute('width', this[0].offsetWidth);//内容+内边距+边框
+    canvas.setAttribute('height', this[0].offsetHeight);
+
+    let _this = this;
+
+    // 用于计算包含关系的画板
+    let canvas2D = canvas.getContext("2d"),
+
+        regionManger = {
+
+            // 绘制（添加）区域范围
+            /**
+             * region_id：区域唯一标识（一个标签上可以维护多个区域）
+             * type：扩展区域类型
+             * data：区域位置数据
+             */
+            "drawer": function (region_id, drawback) {
+                if (regions[region_id] == undefined) regions[region_id] = {
+                    'r': function () {
+                        rgb[0] += 1;
+                        p = 'g';
+                        return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+                    },
+                    'g': function () {
+                        rgb[1] += 1;
+                        p = 'b';
+                        return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+                    },
+                    'b': function () {
+                        rgb[2] += 1;
+                        p = 'r';
+                        return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+                    }
+                }[p]();
+                return _drawerRegion(canvas2D, regions[region_id], drawback, regionManger);
+            },
+
+            // 擦除区域范围
+            "erase": function (drawback) {
+                return _drawerRegion(canvas2D, 'rgb(0,0,0)', drawback, regionManger);
+            },
+
+            // 获取此刻鼠标所在区域
+            "getRegion": function (event) {
+                let pos = _this.position(event), i;
+                pos.x -= _this.css('border-left-width').replace('px', '');
+                pos.y -= _this.css('border-top-width').replace('px', '');
+                let currentRGBA = canvas2D.getImageData(pos.x - 0.5, pos.y - 0.5, 1, 1).data;
+                for (i in regions) {
+                    if ("rgb(" + currentRGBA[0] + "," + currentRGBA[1] + "," + currentRGBA[2] + ")" == regions[i]) {
+                        return [i, pos.x, pos.y];
+                    }
+                }
+                return undefined;
+            }
+        };
+
+    return regionManger;
+
+};
